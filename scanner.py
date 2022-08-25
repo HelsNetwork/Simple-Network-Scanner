0ae9e0bbcc3f80cabb577b29fa71de4d186d0f88
hel- <87879515+hel4s@users.noreply.github.com>
2022-08-25 13:14:12 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,18 +1,26 @@
 from scapy.all import ARP, Ether, srp
 
-target_ip = "IP ADDRESS"
+
+while True:
+       target_ip = (input('Enter an IP address: '))
+
+       if target_ip == "":
+        print("Please enter a valid IP address.")
+
+       else: 
+            break
+   
 # ARP packet
 arp = ARP(pdst=target_ip)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether / arp
-result = srp(packet, timeout=3, verbose=0)[0]
+result = srp(packet, timeout=5)[0]
 clients = []
 
 for sent, received in result:
-    # for each reponse, append ip and mac address tp 'clients' list
-    clients.append({"ip": received.psrc, "mac": received.hwsrc})
-    # print clients
-    print("Available devices on the network:")
-    print("IP" + "MAC")
-    for client in clients:
-        print("{:16}     {}".format(client["ip"], client["mac"]))
+            clients.append({"ip": received.psrc, "mac": received.hwsrc})
+        # print clients
+print("Available devices on the network:")
+print("IP" + "MAC")
+for client in clients:
+            print("{:16}     {}".format(client["ip"], client["mac"]))
